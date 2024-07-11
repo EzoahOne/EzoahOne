a86112aecb575848ee35ddf2c3fd4892ebd7e741
EzoahOne <134557454+EzoahOne@users.noreply.github.com>
2024-07-11 18:55:53 +0000

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,8 @@ app = Flask(__name__)
 
 # Enable logging
 logging.basicConfig(
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
 )
 logger = logging.getLogger(__name__)
 
